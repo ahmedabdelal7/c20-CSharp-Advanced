f31507e3e8b78e5f7a6983dc239d3ecca6be72d2
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-14 19:22:51 +0300

    Task.Run

---
+++ 19- Asynchronous Programming/4-Task.Run Example/2-Win Form Task + Event/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Test_Task.Run
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(351, 256);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(214, 76);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Start Task With New Thread";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(413, 155);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(73, 25);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 533);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
---
+++ 19- Asynchronous Programming/4-Task.Run Example/2-Win Form Task + Event/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Task.Run
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LabelChanged += OnLabelChanged;
+        }
+
+        public class CustomEventArgs : EventArgs
+        {
+            public string Text { get; }
+            public int ThreadID { get; }
+
+            public CustomEventArgs(string text, int threadID)
+            {
+                Text = text;
+                ThreadID = threadID;
+            }
+        }
+
+        public event EventHandler<CustomEventArgs> LabelChanged;
+
+        private async void btn1_Click(object sender, EventArgs e)
+        {
+
+            await ChangeLabel("Hello From UI Thread: ", Thread.CurrentThread.ManagedThreadId);
+
+            MessageBox.Show("Done!");
+        }
+
+        private async Task ChangeLabel(string Text, int ThreadID)
+        {
+            await Task.Delay(5000);
+            
+            lblText.Text = Text+ThreadID;
+
+            CustomEventArgs e = new CustomEventArgs(Text, ThreadID);
+
+            LabelChanged?.Invoke(this,e);// raise event
+        }
+
+        private void OnLabelChanged(object sender, CustomEventArgs e)
+        {
+            MessageBox.Show($"Label Changed To:\n[{e.Text}{e.ThreadID}]");
+
+            BackColor = Color.Yellow;
+        }
+    }
+}
