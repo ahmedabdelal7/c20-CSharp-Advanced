3927a028a75988858e7f489b57b8cef750912083
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-14 19:54:35 +0300

    parallel for

---
+++ 19- Asynchronous Programming/7-Parallel Class/1-Parallel.For/1-instead of a lambda expression/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Define the number of iterations
+        int numberOfIterations = 10;
+
+
+        // Use Parallel.For to execute the loop in parallel
+        Parallel.For(0, numberOfIterations, i =>
+        {
+            Console.WriteLine($"Executing iteration {i} on thread {Task.CurrentId}");
+            // Simulate some work
+            Task.Delay(2000).Wait();
+        });
+
+
+        Console.WriteLine("All iterations completed.");
+        Console.ReadLine();
+    }
+}
