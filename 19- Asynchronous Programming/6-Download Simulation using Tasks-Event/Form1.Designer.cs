cc0b451db3ff47407bdd493a1dfd288301c9ffd5
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-14 19:31:51 +0300

    winForms exp with event

---
+++ 19- Asynchronous Programming/6-Download Simulation using Tasks-Event/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Download_Simulation_using_Tasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(175, 125);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(385, 33);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgress.Location = new System.Drawing.Point(566, 134);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(35, 24);
+            this.lblProgress.TabIndex = 1;
+            this.lblProgress.Text = "0%";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(316, 239);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(128, 47);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnd.Location = new System.Drawing.Point(316, 320);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(128, 47);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(175, 53);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(385, 24);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ 19- Asynchronous Programming/6-Download Simulation using Tasks-Event/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Download_Simulation_using_Tasks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 101;
+            lblProgress.Text = "0%";
+
+            DownloadCompleted += OnDownloadCompleted;
+
+            btnEnd.Enabled = false;
+        }
+
+        //Cancellation Token
+        private CancellationTokenSource _Cts;
+        //Action Event.
+        public event Action DownloadCompleted;
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnEnd.Enabled = true;
+
+            lblStatus.Text = "In Progress";
+            lblProgress.Text = "0%";
+            progressBar1.Value = 0;
+
+            _Cts = new CancellationTokenSource();
+
+            try
+            {
+                await SimulateDownloadAsync(_Cts.Token);
+
+                lblStatus.Text = "Download Completed Successfully.";
+            }
+            catch (OperationCanceledException )
+            {
+
+                lblStatus.Text = "Download Cancelled";
+            }
+            finally
+            {
+                btnStart.Enabled = true;
+                btnEnd.Enabled = false;
+
+                _Cts.Dispose();
+                _Cts=null;
+            }
+        }
+
+        private async Task SimulateDownloadAsync(CancellationToken token)
+        {
+            for (int p = 0; p <= 100; p++)
+            {
+                token.ThrowIfCancellationRequested();
+
+                await Task.Delay(10);
+
+                progressBar1.Value++;
+
+                lblProgress.Text = p.ToString()+"%";
+            }
+            DownloadCompleted?.Invoke();
+        }
+
+
+        private void OnDownloadCompleted()
+        {
+            notifyIcon1.Icon = SystemIcons.Information;
+            //notifyIcon1.Visible = true;
+
+            notifyIcon1.ShowBalloonTip(
+                10000,
+                "Download Status",
+                "Download Completed!",
+                ToolTipIcon.Info
+            );
+        }
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            _Cts.Cancel();
+        }
+
+    }
+}
